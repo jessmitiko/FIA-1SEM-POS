766944a1067026cde46408504583c282e5f5b542
Jessica Mitiko <jessicamitiko@hotmail.com>
2024-11-04 08:49:26 -0300

    feat: add job raw of posicao by linha

---
+++ airflow/dags/ingestion-posicao_linhas.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 11, 4),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'catchup': False,
+    'retries': 1
+}
+
+dag = DAG('ingestion-posicao_linhas', default_args=default_args, schedule_interval='@daily') #*/10 * * * *
+
+ingestion_raw_posicao_by_linha = SparkSubmitOperator(
+    application="/opt/airflow/jobs/bronze/ingestion-posicao_by_linha.py", 
+    task_id="ingestion-raw-posicao_by_linha",
+    packages="org.apache.hadoop:hadoop-aws:3.3.2",
+    env_vars={
+        "AWS_ACCESS_KEY_ID": "{{ var.value.AWS_ACCESS_KEY_ID }}",
+        "AWS_SECRET_ACCESS_KEY": "{{ var.value.AWS_SECRET_ACCESS_KEY }}",
+        "S3_ENDPOINT": "{{ var.value.S3_ENDPOINT }}"
+    },
+    dag=dag
+)
+
+# ingestion_silver_terminal_linhas = SparkSubmitOperator(
+#     application="/opt/airflow/jobs/silver/ingestion-linhas.py", 
+#     task_id="ingestion-silver-terminal_linhas",
+#     packages="org.apache.hadoop:hadoop-aws:3.3.2",
+#     env_vars={
+#         "AWS_ACCESS_KEY_ID": "{{ var.value.AWS_ACCESS_KEY_ID }}",
+#         "AWS_SECRET_ACCESS_KEY": "{{ var.value.AWS_SECRET_ACCESS_KEY }}",
+#         "S3_ENDPOINT": "{{ var.value.S3_ENDPOINT }}"
+#     },
+#     dag=dag
+# )
+
+ingestion_raw_posicao_by_linha
---
+++ airflow/jobs/bronze/ingestion-posicao_by_linha.py
@@ -0,0 +1,58 @@
+import os
+import json
+import boto3
+import requests
+from datetime import datetime
+from pyspark.sql import SparkSession
+
+base_url='https://api.olhovivo.sptrans.com.br/v2.1'
+token='bde4a3deabdd81f9206e0a69974a1a8eb18425137adbadf8e9cccf9d45ba53bd' #
+
+s3_client=boto3.client(
+    's3',
+    endpoint_url=os.getenv("S3_ENDPOINT"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    config=boto3.session.Config(signature_version='s3v4')
+)
+
+def save_to_s3(path_s3, data):
+    bucket_name, key = path_s3.replace("s3a://", "").split('/', 1)
+    current_timestamp=datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    print(f'saving {len(data)} items to {bucket_name} bucket in {key}dt_ingestion={current_timestamp}/data.json')
+    s3_client.put_object(Body=(bytes(json.dumps(data, default=str).encode('UTF-8'))), Bucket=bucket_name, Key=f'{key}dt_ingestion={current_timestamp}/data.json')
+
+    print('done!')
+
+if __name__ == "__main__":
+    spark = SparkSession.builder \
+        .appName("ingestion-linhas") \
+        .master("local[*]") \
+        .config("spark.hadoop.fs.s3a.endpoint", os.getenv("S3_ENDPOINT")) \
+        .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID")) \
+        .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY")) \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+        .getOrCreate()
+    
+    response=requests.post(f'{base_url}/Login/Autenticar?token={token}')
+    headers={"Cookie": response.headers['Set-Cookie']}
+
+    linhas=spark.read.option('header', 'true').csv('s3a://silver/linhas/csv/')
+    linhas=linhas.select('cl').limit(50).collect() #codigo linhas
+
+    posicao_by_linha=[]
+    for row in linhas:
+        cl=row['cl']
+        
+        print(f'getting posicao of linha {cl}... ')
+        response=requests.get(f'{base_url}/Posicao/Linha?codigoLinha={cl}', headers=headers)
+        
+        posicao=response.json()
+
+        posicao['cl']=cl
+        posicao_by_linha.append(posicao)
+
+    path_s3='s3a://bronze/posicao_by_linha/'
+    save_to_s3(path_s3, posicao_by_linha)
